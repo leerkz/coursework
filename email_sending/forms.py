539a00789a8a1efc2d0d0f3820c572cb280c8959
lerakuzmina <l3ra131007@gmail.com>
2025-03-19 17:51:39 +0300

    add views for recipient

---
+++ email_sending/forms.py
@@ -0,0 +1,39 @@
+from django.core.exceptions import ValidationError
+from django.forms import BooleanField, ModelForm
+
+from email_sending.models import EmailManagement, EmailRecipient, Sending
+
+
+class StyleFormMixin:
+    def init(self, *args, **kwargs):
+        super().init(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class EmailManagementForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = EmailManagement
+        fields = '__all__'
+
+        def init(self, *args, **kwargs):
+            self.user = kwargs.pop("user", None)  # Получаем текущего получателя
+            super().init(*args, **kwargs)
+
+class EmailRecipientForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = EmailRecipient
+        fields = '__all__'
+
+class SendingForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Sending
+        fields = '__all__'
+
+        def init(self, *args, **kwargs):
+            self.user = kwargs.pop("user", None)  # Получаем текущего получателя
+            super().init(*args, **kwargs)
+
--- email_sending/views.py
+++ email_sending/views.py
@@ -1,15 +1,27 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from email_sending.models import EmailRecipient
 
 
 class EmailRecipientCreateView(CreateView):
     model = EmailRecipient
-    #form_class = EmailRecipientForm
     template_name = 'email_sending/recipient_form.html'
+    fields = ['email', 'full_name', 'comment']
     success_url = reverse_lazy('email_sending:home')
 
-class HomePageView(ListView):
+class EmailRecipientDetailView(DetailView):
     model = EmailRecipient
-    template_name = 'email_sending/home.html'
+    template_name = 'email_sending/recipient_detail.html'
+    context_object_name = 'recipient'
+
+class EmailRecipientUpdateView(UpdateView):
+    model = EmailRecipient
+    template_name = 'email_sending/recipient_form.html'
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('email_sending:home')
+
+class EmailRecipientDeleteView(DeleteView):
+    model = EmailRecipient
+    template_name = 'email_sending/recipient_delete.html'
+    success_url = reverse_lazy('email_sending:home')
